9b4bf1f32fe8bf53ca068112bbdcbea4ef45c764
EmptyException <unityamvs@gmail.com>
2019-04-15 15:41:31 -0300

    Update to 0.3.5

--- index.js
+++ index.js
@@ -1,84 +1,81 @@
 const { promisify } = require('util');
 const execAsync = promisify(require('child_process').exec);
+const { join } = require('path');
+const request = require('./request');
+const fs = require('fs');
 class Oneesama {
-    /**
-     * @param {string} [dir='./oppai'] oppai folder path, leave it null if you have the oppai folder in your PATH.
-     */
-	constructor(dir = './oppai') {
-		if(dir) {
-			this.dir = dir;
-		} else {
-			this.dir = 'oppai';
-		}
+	/**
+	 * @param {string} [dir='./oppai'] oppai folder path, leave it null if you have the oppai folder in your PATH.
+	 * @param {string} shell Shell to use to run bash commands
+	 */
+	constructor({ shell, oppaiDir = 'oppai', tempFolder = 'beatmaps' }) {
+		this.shell = shell;
+		this.oppaiDir = oppaiDir;
+		this.tempFolder = join(process.cwd(), tempFolder);
+		this.execOptions = {
+			windowsHide: true
+		};
+		if (shell) this.execOptions.shell = shell;
 	}
-    /**
-     * @typedef {Object} OppaiData
-     * @property {string} version beatmap diff name
-     * @property {string} title beatmap title
-     * @property {string} fullTitle beatmap full title
-     * @property {number} stars beatmap star rate
-     * @property {string} artist beatmap artist name
-     * @property {string} creator beatmap author username
-     * @property {number} maxCombo beatmap max combo
-     * @property {number} numCircles number of hitcircles.
-     * @property {number} numSliders number of sliders.
-     * @property {number} numSpinners number of spinners.
-     * @property {Array<number>} pp pp raw value for all accs (sum of aim pp, acc pp and speed pp).
-     * @property {number} aimPp aim pp value
-     * @property {number} speedPp speed pp value.
-     * @property {number} accPp acc pp value
-     * @property {Array<number>} accs All accs values being calculated.
-     * @property {number} cs circle size value
-     * @property {number} od overral difficulty value.
-     * @property {number} ar approach rate value
-     * @property {number} hp Drain rate value.
-     */
-    /**
-     * Get oppai data from the pure oppai binding.
-     * @param {string} id beatmap id
-     * @param {?Array<number>} [accs=[100]] accuracy values
-     * @return {Promise<OppaiData>}
-     */
+
+	/**
+	 * Get oppai data from CLI
+	 * @param {string} id beatmap id
+	 * @param {?Array<number>} [accs=[100]] accuracy values
+	 * @return {Promise<OppaiData>}
+	 */
 	async get(id, accs = [100]) {
+		const data = await request(`https://osu.ppy.sh/osu/${id}`);
 		try {
-			const child = await execAsync(`curl https://osu.ppy.sh/osu/${id} | ${this.dir} - -ojson`, { windowsHide: true });
-			const totalPPList = [];
-			const promises = [];
-			for(const acc of accs) {
-				promises.push(execAsync(`curl https://osu.ppy.sh/osu/${id} | ${this.dir} - ${acc}% -ojson`,
-				{ windowsHide: true }));
-			}
-			const allData = await Promise.all(promises);
-			for(const proc of allData) {
-				const result = JSON.parse(proc.stdout);
-				totalPPList.push(result.pp);
-			}
-			const b = JSON.parse(child.stdout);
-			const oppaiJson = {
-				version: b.version,
-				title: b.title,
-				fullTitle: `${b.artist} - ${b.title}`,
-				stars: b.stars,
-				artist: b.artist,
-				creator: b.creator,
-				maxCombo: b.max_combo,
-				numCircles: b.num_circles,
-				numSliders: b.num_sliders,
-				numSpinners: b.num_spinners,
-				aimPp: b.aim_pp,
-				speedPp: b.speed_pp,
-				accPp: b.acc_pp,
-				accs,
-				pp: totalPPList,
-				cs: b.cs,
-				od: b.od,
-				ar: b.ar,
-				hp: b.hp
-			};
-			return oppaiJson;
-		} catch(err) {
-			throw err;
+			fs.mkdirSync(this.tempFolder);
+		} catch (e) {
+			// ignored
 		}
+		const filePath = join(this.tempFolder, `${id}.osu`).replace(/\\/g, '/');
+		fs.writeFileSync(filePath, data);
+		const child = await execAsync(
+			`cat ${filePath} | ${this.oppaiDir} - -ojson`,
+			this.execOptions
+		);
+		const totalPPList = [];
+		const promises = [];
+		for (const acc of accs) {
+			promises.push(
+				execAsync(
+					`cat ${filePath} | ${this.oppaiDir} - ${acc}% -ojson`,
+					this.execOptions
+				)
+			);
+		}
+		const allData = await Promise.all(promises);
+		for (const proc of allData) {
+			const result = JSON.parse(proc.stdout);
+			totalPPList.push(result.pp);
+		}
+		const b = JSON.parse(child.stdout);
+		const oppaiJson = {
+			version: b.version,
+			title: b.title,
+			fullTitle: `${b.artist} - ${b.title}`,
+			stars: b.stars,
+			artist: b.artist,
+			creator: b.creator,
+			maxCombo: b.max_combo,
+			numCircles: b.num_circles,
+			numSliders: b.num_sliders,
+			numSpinners: b.num_spinners,
+			aimPp: b.aim_pp,
+			speedPp: b.speed_pp,
+			accPp: b.acc_pp,
+			accs,
+			pp: totalPPList,
+			cs: b.cs,
+			od: b.od,
+			ar: b.ar,
+			hp: b.hp
+		};
+		fs.unlinkSync(filePath);
+		return oppaiJson;
 	}
 }
 
--- test/index.js
+++ test/index.js
@@ -1,27 +1,24 @@
 const Oneesama = require('../index');
-const parser = new Oneesama();
-parser.get('1639629', [95, 97, 100]).then(data => {
-    // You will also need to download the C++ version from here https://github.com/Francesco149/oppai-ng
-    /*
-    { version: 'For Victory',
-      title: 'Upon My Honor',
-      stars: 5.417534676043834,
-      artist: 'Whispered',
-      creator: 'Faputa',
-      maxCombo: 3535,
-      numCircles: 3535,
-      numSliders: 8,
-      numSpinners: 14,
-      aimPp: 0,
-      speedPp: 143.169681355677,
-      accPp: 145.9120243677375,
-      pp: 298.57186759157696,
-      cs: 2,
-      od: 6.3,
-      ar: 10,
-      hp: 3.7
-    }
-    */
-   // eslint-disable-next-line no-console
-	console.log(data);
+const parser = new Oneesama({
+	oppaiDir: './oppai.exe',
+	shell: 'C:\\Program Files\\Git\\bin\\bash.exe'
 });
+parser
+	.get('1736173', [
+		96,
+		97,
+		98,
+		99,
+		99.1,
+		99.2,
+		99.3,
+		99.4,
+		99.5,
+		99.6,
+		99.7,
+		100
+	])
+	.then(data => {
+		// You will also need to download the C version from here https://github.com/Francesco149/oppai-ng
+		console.log(data);
+	});
